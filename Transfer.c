675a46b90721278ace22663d7e6d1e0e2ebe4561
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-11-09 22:00:45 +0100

    Modify the Transfer function

--- Transfer.c
+++ Transfer.c
@@ -10,23 +10,12 @@ void Transfer(void)
 {
 
     printf("TRANSFER\n");
-    int proceed, i, j, BankChoice, perform;
+    int i, j, BankChoice;
 
-    printf("Enter 1 to proceed\nOr 0 to exit\n: ");
-    scanf("%d", &proceed);
-
-    if (proceed == 1)
-    {
-        ;
-    }
-    else if (proceed == 0)
-    {
-        return;
-    }
 Bank:
-    printf("\nSelect your Recipient's Bank\n");
-    char BankName[6][30] = {"Sterling", "UBA", "First Bank", "Access Bank", "Heritage Bank", "GT Bank"};
-    for (i = 0; i < 6; i++)
+    printf("\nSELECT RECIPIENT'S BANK\n");
+    char BankName[7][30] = {"Sterling", "UBA", "First Bank", "Access Bank", "Heritage Bank", "GT Bank", "Exit"};
+    for (i = 0; i < 7; i++)
     {
         printf("%d. %s", i + 1, BankName[i]);
         printf("\n");
@@ -34,72 +23,49 @@ Bank:
     printf("\n: ");
 
     scanf("%d", &BankChoice);
-    if (BankChoice > 6 || BankChoice < 1)
+    if (BankChoice > 7 || BankChoice < 1)
     {
         printf("Invalid Selection");
         goto Bank;
     }
+    if (BankChoice == 7)
+            return;
 
     /** Using the strlen fun to test acc no validity*/
 
     char AccNo[13] = {0};
+
     do
     {
-        printf("\nEnter Recipient's Account No\n:  ");
+        printf("\nENTER RECIPIENT'S ACCOUNT NO\n:  ");
         scanf("%13s", AccNo);
         if (strlen(AccNo) != 10)
         {
-            printf("\nInvlaid Account Number!\n\n");
+            printf("\nInvalid Account Number!\n\n");
         }
     } while (strlen(AccNo) != 10);
 
     float TransferAmount;
 
-    printf("\nEnter Amount to Transfer\n: ");
+    printf("\nENTER AMOUNT TO TRANSFER\n: ");
     scanf("%f", &TransferAmount);
 
     if (TransferAmount > Balance)
     {
         printf("Insufficient Funds\nYour Balance is %.2f\n", Balance);
-        goto Perform;
+        return;
     }
     int confirm;
 
-    printf("\nConfirm Transfer \n1. Yes \n2. No\n: ");
+    printf("\nCONFIRM TRANSFER\n1. Yes \n2. No\n: ");
     scanf("%d", &confirm);
     if (confirm == 1)
     {
         /* The global var Balance is in use here to get the value of the balance after transfer confirmation */
         Balance -= TransferAmount;
-        printf("\nTransfer Successful!\n");
+        printf("\nTRANSFER SUCCESSFUL!\n");
         printf("You have successfully transferrred %.2f to %s, %s\n\n", TransferAmount, AccNo, BankName[BankChoice - 1]);
-
-    Perform:
-        printf("Enter 1 to perform another transfer or 2 to exit\n: ");
-        scanf("%d", &perform);
-
-        if (perform == 1)
-        {
-            Transfer();
-        }
-        else if (perform == 2)
-        {
-            return;
-        }
-    }
+     }
     else if (confirm == 2)
-        ;
-    {
-        printf("\nDo you want to Perform another transfer\n1 to perform another transfer\n2 to exit\n: ");
-        scanf("%d", &perform);
-
-        if (perform == 1)
-        {
-            Transfer();
-        }
-        else if (perform == 2)
-        {
             return;
-        }
-    }
 }
